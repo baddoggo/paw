e7c2f8ede4e7d770da74bed3e7178396b2d4916d
baddoggo <pixeltentacles@gmail.com>
2020-03-04 19:11:58 +0300

    owo files

---
+++ precache-manifest.39c6f47ba1122838b7fbef75f28cf65e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91f105ed76eef9bfa181e64652a4614",
+    "url": "/pawpaw/index.html"
+  },
+  {
+    "revision": "44441b699882e3e3a148",
+    "url": "/pawpaw/static/css/main.211f612c.chunk.css"
+  },
+  {
+    "revision": "8e822f4d197cb58be744",
+    "url": "/pawpaw/static/js/2.b6d55dc4.chunk.js"
+  },
+  {
+    "revision": "44441b699882e3e3a148",
+    "url": "/pawpaw/static/js/main.fcf6866a.chunk.js"
+  },
+  {
+    "revision": "371c2fa66f297119587a",
+    "url": "/pawpaw/static/js/runtime~main.9f8d1c47.js"
+  },
+  {
+    "revision": "ab4323a88ed383dfac725f10787862d5",
+    "url": "/pawpaw/static/media/NormalMap.ab4323a8.png"
+  },
+  {
+    "revision": "35bfb10e2cf58faa67e3910632ef846a",
+    "url": "/pawpaw/static/media/paw.35bfb10e.png"
+  }
+]);
